06b5d55c2af8b378ce11f49d1828f32209435340
Antoine <44494400+Anthoxoo@users.noreply.github.com>
2025-11-15 14:32:25 +0100

    changed to run with python3
    used to run it using conda but realized that not everybody uses it.

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 DIR_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-CONDA_EXEC="conda"
-$CONDA_EXEC run -n base python "$DIR_PATH/main.py"
+python3 -O "$DIR_PATH/main.py"
